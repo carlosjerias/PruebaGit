772f7a26f0f0fab5491d224474cba09bac828f71
cjerias <carlosjerias@gmail.com>
2017-07-18 15:58:01 -0400

    Agregar archivos de proyecto.

---
+++ EjemploGIT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EjemploGIT.Startup))]
+namespace EjemploGIT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
